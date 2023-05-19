cfb079cdf64d0edf53a4a7c70cb90f572939d563
Mirza Telalovic <mirza.telalovic@gmail.com>
2023-05-19 13:37:15 +0200

    add entity framework support, add simple table migration, ajust project configurations

--- GoGreen/Program.cs
+++ GoGreen/Program.cs
@@ -1,25 +1,15 @@
+using GoGreen;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services); // calling ConfigureServices method
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+startup.Configure(app, builder.Environment);
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
